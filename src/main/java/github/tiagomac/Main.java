9f60b93f2819dfaa00ebc18ad6339bda144b337e
Tiago Mesquita <tiagomac@gmail.com>
2019-08-13 14:52:12 -0300

    CEP adjust

--- src/main/java/github/tiagomac/Main.java
+++ src/main/java/github/tiagomac/Main.java
@@ -8,11 +8,13 @@ import github.tiagomac.ws.correios.EnderecoERP;
 
 public class Main {
 
+	private static final String WSDL_URL = "https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl";
+
 	public static void main(String[] args) {
 		try {
-			URL url = new URL("https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl");
+			URL url = new URL(WSDL_URL);
 			AtendeClienteService atendeClienteService = new AtendeClienteService(url);
-			EnderecoERP consultaCEP = atendeClienteService.getAtendeClientePort().consultaCEP("40279080");
+			EnderecoERP consultaCEP = atendeClienteService.getAtendeClientePort().consultaCEP("70150903");
 			System.out.println(consultaCEP);
 		} catch (Exception e) {
 			e.printStackTrace();
